719a0bc761c913cdb55019c3a5dff67cce173db2
Geethika Singareddy <geethika1129@gmail.com>
2022-02-14 09:23:11 +0530

    added homepage

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.contrib import admin
+from django.urls import path
+from home import views
 
 # Create your views here.
+def index(request):
+    return HttpResponse("this is homepage")
