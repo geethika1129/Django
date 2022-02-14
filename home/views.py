7cff28fa0ee7edc7aac3e831ed9816a3d233df4c
Geethika Singareddy <geethika1129@gmail.com>
2022-02-14 09:36:11 +0530

    url dispatching

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ from home import views
 # Create your views here.
 def index(request):
     return HttpResponse("this is homepage")
+def about(request):
+    return HttpResponse("this is aboutpage")
+def services(request):
+    return HttpResponse("this is servicespage")
+def contact(request):
+    return HttpResponse("this is contactpage")
