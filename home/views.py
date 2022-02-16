fe5a489764a264bde98c249a607b2157dbd9e005
Geethika Singareddy <geethika1129@gmail.com>
2022-02-16 15:04:24 +0530

    added object

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,HttpResponse
-from django.contrib import admin
-from django.urls import path
-from home import views
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -14,4 +13,12 @@ def services(request):
     return render(request,'services.html')
     
 def contact(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        desc=request.POST.get('desc')
+        contact = Contact(name=name, email=email, desc=desc,date=datetime.today())
+        contact.save()
+
+
     return render(request,'contact.html')
