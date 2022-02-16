810d937ec74f8b962ab12be5ce158e02e0356462
Geethika Singareddy <geethika1129@gmail.com>
2022-02-16 15:45:44 +0530

    added messages framework

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib.messages import constants as messages
 
 # Create your views here.
 def index(request):
@@ -19,6 +20,7 @@ def contact(request):
         desc=request.POST.get('desc')
         contact = Contact(name=name, email=email, desc=desc,date=datetime.today())
         contact.save()
+        messages.success(request, 'Your message has been sent successfully.')
 
 
     return render(request,'contact.html')
